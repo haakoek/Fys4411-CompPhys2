d4c54e52c2bd6bc1cd21bf8fbbf9fcb400d9c3df
hakii27 <haakoek@student.matnat.uio.no>
2018-02-11 15:59:24 +0100

    Added VMC for bosons project

---
+++ Project1_VMC_Bosons/src/steepestdescent.cpp
@@ -0,0 +1,230 @@
+#include "examples.h"
+#include <iostream>
+#include <iomanip>
+#include "system.h"
+#include "particle.h"
+#include "WaveFunctions/wavefunction.h"
+#include "WaveFunctions/simplegaussian.h"
+#include "WaveFunctions/interactinggaussian.h"
+#include "WaveFunctions/heliumwavefunction.h"
+#include "Hamiltonians/heliumhamiltonian.h"
+#include "Hamiltonians/hamiltonian.h"
+#include "Hamiltonians/harmonicoscillator.h"
+#include "InitialStates/initialstate.h"
+#include "InitialStates/randomuniform.h"
+#include "Math/random.h"
+#include <ctime>
+#include "Hamiltonians/ellipticinteractingoscillator.h"
+#include "steepestdescent.h"
+#include <cmath>
+
+using namespace std;
+
+SteepestDescent::SteepestDescent(System *system, int nSteps, int n_particles, int n_dim)
+{
+    m_system = system;
+    m_nSteps = nSteps;
+    m_iterations = 0;
+    m_nparticles = n_particles;
+    m_numberOfDimensions = n_dim;
+}
+
+double SteepestDescent::findOptimalAlpha(double alpha) {
+    double E_new;
+    double alpha_new;
+    //double beta_new;
+
+    double gamma_alpha = 0.3;    //step size (learning rate)
+    //double gamma_beta  = 0.3;
+
+    int MAX_ITER = 100;  //maximum number of iterations
+    int iters_sinceLastImprovement = 0;
+
+    int iter = 0;
+
+    double alpha_old = alpha;
+    //double beta_old  = parameters[1];
+
+    m_system->setAlpha(alpha_old);
+    //m_system->setBeta(beta_old);
+
+    m_system->runMetropolisSteps(m_nSteps);
+
+    m_system->printToTerminal();
+
+    double gradient_old, gradient_new;
+
+    gradient_old = m_system->getAlphaDerivativeEnergy();
+    //gradient_old[1] = m_system->getBetaDerivativeEnergy();
+
+    double E_old = m_system->getEnergy();
+
+    cout << E_old << endl;
+
+    m_system->getSampler()->reset();
+
+    while(iter < MAX_ITER && iters_sinceLastImprovement < 20) {
+
+        iter+=1;
+
+        cout << "***********" << endl;
+        cout << "Iteration: " << iter << endl;
+
+        alpha_new = alpha_old - gamma_alpha*gradient_old;
+
+        //m_system->setInitialState(new RandomUniform(m_system, m_numberOfDimensions, m_nparticles));
+
+        m_system->setAlpha(alpha_new);
+
+        m_system->setInitialState(new RandomUniform(m_system,m_numberOfDimensions,m_nparticles));
+        m_system->runMetropolisSteps((int) m_nSteps);
+
+        cout << "Acceptance_new: " << m_system->getSampler()->getAcceptanceRate() << endl;
+
+        E_new = m_system->getEnergy();
+
+        gradient_new = m_system->getAlphaDerivativeEnergy();
+
+        if(gradient_new*gradient_new > gradient_old*gradient_old) {
+            if(iters_sinceLastImprovement == 10) {
+                gamma_alpha = 0.03;
+            } else {
+                gamma_alpha *= 0.9;
+            }
+            iters_sinceLastImprovement += 1;
+        } else {
+
+            iters_sinceLastImprovement = 0;
+
+            alpha_old = alpha_new;
+            gradient_old = gradient_new;
+
+            cout << "alpha_old: " << alpha_old << endl;
+            cout << "alpha_new: " << alpha_new << endl;
+            cout << "E_old: "     << E_old << endl;
+            cout << "E_new: " << E_new << endl;
+
+            E_old = E_new;
+
+        }
+        cout << "***********" << endl;
+        m_system->getSampler()->reset();
+
+
+    }
+
+    m_optimalAlpha = alpha_old;
+
+    cout << "Optimal alpha: " << m_optimalAlpha << endl;
+
+    return m_optimalAlpha;
+
+}
+
+void SteepestDescent::altOptimize(std::vector<double> parameters) {
+        double E_new;
+        double alpha_new;
+        double beta_new;
+
+        double gamma_alpha = 0.3;    //step size (learning rate)
+        double gamma_beta  = 0.3;
+
+        int MAX_ITER = 100;  //maximum number of iterations
+        int iters_sinceLastImprovement = 0;
+
+        int iter = 0;
+
+        double alpha_old = parameters[0];
+        double beta_old  = parameters[1];
+
+        m_system->setAlpha(alpha_old);
+        m_system->setBeta(beta_old);
+
+        m_system->runMetropolisSteps(m_nSteps);
+
+        m_system->printToTerminal();
+
+        std::vector<double> gradient_old(2);
+        std::vector<double> gradient_new(2);
+
+        gradient_old[0] = m_system->getAlphaDerivativeEnergy();
+        gradient_old[1] = m_system->getBetaDerivativeEnergy();
+
+        double E_old = m_system->getEnergy();
+
+        cout << E_old << endl;
+
+        m_system->getSampler()->reset();
+
+        while(iter < MAX_ITER && iters_sinceLastImprovement < 20) {
+
+            iter+=1;
+
+            cout << "***********" << endl;
+            cout << "Iteration: " << iter << endl;
+
+            alpha_new = alpha_old - gamma_alpha*gradient_old[0];
+            beta_new  = beta_old  - gamma_beta*gradient_old[1];
+
+            //m_system->setInitialState(new RandomUniform(m_system, m_numberOfDimensions, m_nparticles));
+
+            m_system->setAlpha(alpha_new);
+            m_system->setBeta(beta_new);
+
+            m_system->setInitialState(new RandomUniform(m_system,m_numberOfDimensions,m_nparticles));
+            m_system->runMetropolisSteps((int) m_nSteps);
+
+            cout << "Acceptance_new: " << m_system->getSampler()->getAcceptanceRate() << endl;
+
+            E_new = m_system->getEnergy();
+
+            gradient_new[0] = m_system->getAlphaDerivativeEnergy();
+            gradient_new[1] = m_system->getBetaDerivativeEnergy();
+
+            if(alpha_old < 0 || beta_old < 0 || alpha_new < 0 || beta_new < 0) {
+                cout << "balle" << endl;
+            }
+
+            if(gradient_new[0]*gradient_new[0]+gradient_new[1]*gradient_new[1] > gradient_old[0]*gradient_old[0]+gradient_old[1]*gradient_old[1]) {
+                gamma_alpha *= 0.5;
+                gamma_beta  *= 0.5;
+                iters_sinceLastImprovement += 1;
+            } else {
+
+                iters_sinceLastImprovement = 0;
+
+                alpha_old = alpha_new;
+                beta_old  = beta_new;
+                gradient_old[0] = gradient_new[0];
+                gradient_old[1] = gradient_new[1];
+
+
+                cout << "alpha_old: " << alpha_old << endl;
+                cout << "beta_old:  " << beta_old  << endl;
+
+                cout << "alpha_new: " << alpha_new << endl;
+                cout << "beta_new:  " << beta_new  << endl;
+
+                cout << "E_old: "     << E_old << endl;
+                cout << "E_new: " << E_new << endl;
+
+                E_old = E_new;
+
+            }
+            cout << "***********" << endl;
+            m_system->getSampler()->reset();
+
+
+        }
+
+        m_optimalAlpha = alpha_old;
+        m_optimalBeta  = beta_old;
+
+        cout << "Optimal alpha: " << m_optimalAlpha << endl;
+        cout << "Optimal beta:  " << m_optimalBeta  << endl;
+}
+
+int SteepestDescent::getNrOfIters() {
+    return m_iterations;
+}
+
